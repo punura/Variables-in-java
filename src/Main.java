64d9ad21d45c1320079af090213eeb99d75e2944
Punura Wathsara <123pemmawadupunura@gmail.com>
2022-06-03 08:30:32 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int x = 123;
+		double y = 3.14;
+		boolean z = true;
+		char symbol = '@';
+		String name = "Tom";
+		
+		System.out.println("Hello "+name);
+		System.out.println("Number is "+x);
+	}
+
+}
